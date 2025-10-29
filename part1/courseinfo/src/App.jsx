abca68ae5a699fe6f971c6354134ef968ffa8078
Karim Mohamed <132220452+flutter-karim@users.noreply.github.com>
2025-10-29 19:41:08 +0300

    Exercises 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,56 +1,50 @@
 const Header = (props) => {
-  console.log(props)
+  console.log(props);
   return <h1>{props.course}</h1>;
-}
+};
 
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercises}
+      {props.part.name} {props.part.exercises}
     </p>
-  )
-}
+  );
+};
 
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1.name} exercises={props.part1.exercises} />
-      <Part part={props.part2.name} exercises={props.part2.exercises} />
-      <Part part={props.part3.name} exercises={props.part3.exercises} />
+      {props.parts.map((part) => (
+        <Part key={part.name} part={part} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
+    <p>
+      Number of exercises{" "}
+      {props.parts.reduce((sum, part) => sum + part.exercises, 0)}
+    </p>
   );
-}
-
+};
 
 const App = () => {
-
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const course = "Half Stack application development";
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
