26e44c92efb03e8ac7f4f0a11e7dad196d2098cb
Karim Mohamed <132220452+flutter-karim@users.noreply.github.com>
2025-10-30 09:56:39 +0300

    Exercises 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
 function App() {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-12345678" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handelName = (e) => {
     setNewName(e.target.value);
+  };
+  const handelNumber = (e) => {
+    setNewNumber(e.target.value);
   };
 
   const handelSubmit = (e) => {
@@ -14,8 +20,9 @@ function App() {
     if (names.indexOf(newName.toLowerCase()) !== -1) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons([...persons, { name: newName }]);
+      setPersons([...persons, { name: newName, number: newNumber }]);
       setNewName("");
+      setNewNumber("");
     }
   };
   return (
@@ -24,7 +31,12 @@ function App() {
         <h2>Phonebook</h2>
         <form onSubmit={handelSubmit}>
           <div>
-            name: <input value={newName} onChange={handelName} />
+            <div>
+              name: <input value={newName} onChange={handelName} />
+            </div>
+            <div>
+              number: <input value={newNumber} onChange={handelNumber} />
+            </div>
           </div>
           <div>
             <button type="submit">add</button>
@@ -32,7 +44,9 @@ function App() {
         </form>
         <h2>Numbers</h2>
         {persons.map((person, i) => (
-          <p key={i}>{person.name}</p>
+          <p key={i}>
+            {person.name} {person.number}
+          </p>
         ))}
       </div>
     </>
