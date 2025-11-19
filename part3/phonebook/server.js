3775ececc686044eda9f79ca646ce3a54cbd1747
Karim Mohamed <132220452+flutter-karim@users.noreply.github.com>
2025-11-19 13:07:33 +0200

    3.14

--- part3/phonebook/server.js
+++ part3/phonebook/server.js
@@ -25,7 +25,8 @@ app.get("/api/persons", async (req, res) => {
   res.json(persons);
 });
 
-app.get("/info", (req, res) => {
+app.get("/info", async (req, res) => {
+  const persons = await PersonModel.find({});
   const currentDate = new Date();
   res.send(
     `<p>Phonebook has info for ${persons.length} people</p>${currentDate}`
@@ -55,9 +56,8 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(200).send("deleted");
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", async (req, res) => {
   const { name, number } = req.body;
-  const newId = Math.floor(Math.random() * 1000000000).toString();
 
   if (!name) {
     return res.status(400).json({ error: "name is missing" });
@@ -67,27 +67,14 @@ app.post("/api/persons", (req, res) => {
     return res.status(400).json({ error: "number is missing" });
   }
 
-  const nameIndex = persons.findIndex((user) => user.name === name);
-
-  if (nameIndex !== -1) {
-    return res.status(400).json({ error: "name must be unique" });
-  }
-
-  const numberIndex = persons.findIndex((user) => user.number === number);
-
-  if (numberIndex !== -1) {
-    return res.status(400).json({ error: "number must be unique" });
-  }
-
   const newPerson = {
-    id: newId,
     name,
     number,
   };
 
-  persons.push(newPerson);
+  const person = await PersonModel.create(newPerson);
 
-  res.status(201).send(newPerson);
+  res.status(201).send(person);
 });
 
 const PORT = process.env.PORT || 5000;
