a89bac77b567729e0f476926bfa59ea47fafcf2c
Karim Mohamed <132220452+flutter-karim@users.noreply.github.com>
2025-11-20 15:54:05 +0200

    4.3 : 4.7

--- part3/blog/tests/blog.test.js
+++ part3/blog/tests/blog.test.js
@@ -1,6 +1,11 @@
 import { test, describe } from "node:test";
 import assert from "node:assert";
-import { dummy, totalLikes } from "../utils/list_helper.js";
+import {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+} from "../utils/list_helper.js";
 
 test("dummy returns one", () => {
   const blogs = [];
@@ -61,5 +66,91 @@ describe("total likes", () => {
 
     const result = totalLikes(blogs);
     assert.strictEqual(result, 21);
+  });
+});
+
+describe("favorite blog", () => {
+  test("of a bigger list is the one with most likes", () => {
+    const blogs = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful222",
+        author: "Edsger W. Dijkstra",
+        url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+        likes: 15,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful3333333",
+        author: "Edsger W. Dijkstra",
+        url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+        likes: 1,
+        __v: 0,
+      },
+    ];
+
+    const result = favoriteBlog(blogs);
+    const expected = {
+      title: "Go To Statement Considered Harmful222",
+      likes: 15,
+    };
+
+    assert.deepStrictEqual(result, expected);
+  });
+});
+
+describe("top author by posts", () => {
+  test("author with most blogs", () => {
+    const blogs = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful222",
+        author: "Edsger W. Dijkstra",
+        url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+        likes: 15,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful3333333",
+        author: "Edsger W. Dijkstra",
+        url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+        likes: 1,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful3333333",
+        author: "Karim Mohamed",
+        url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+        likes: 1,
+        __v: 0,
+      },
+    ];
+
+    const result = mostBlogs(blogs);
+    const expected = {
+      author: "Edsger W. Dijkstra",
+      blogs: 3,
+    };
+
+    assert.deepStrictEqual(result, expected);
   });
 });
--- part3/blog/utils/list_helper.js
+++ part3/blog/utils/list_helper.js
@@ -5,3 +5,37 @@ export const dummy = (blogs) => {
 export const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
+
+export const favoriteBlog = (blogs) => {
+  const fav = blogs.reduce((max, blog) =>
+    blog.likes > max.likes ? blog : max
+  );
+
+  return {
+    title: fav.title,
+    likes: fav.likes,
+  };
+};
+
+export const mostBlogs = (blogs) => {
+  const counts = {};
+
+  blogs.forEach((blog) => {
+    counts[blog.author] = (counts[blog.author] || 0) + 1;
+  });
+
+  let topAuthor = null;
+  let maxBlogs = 0;
+
+  for (const author in counts) {
+    if (counts[author] > maxBlogs) {
+      maxBlogs = counts[author];
+      topAuthor = author;
+    }
+  }
+
+  return {
+    author: topAuthor,
+    blogs: maxBlogs,
+  };
+};
